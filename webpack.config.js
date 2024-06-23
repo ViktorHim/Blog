96e53f7473fa7c69efa1c6704b78c5a4bb3beb64
ViktorHim <vhimlihmail@gmail.com>
2024-06-23 18:46:20 +0300

    add-html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development',
@@ -7,4 +9,10 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'build'),
   },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html"),
+        }),
+        new webpack.ProgressPlugin(),
+    ],
 };
